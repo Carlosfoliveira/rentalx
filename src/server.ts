a634bde8acf528a8831c6ce3c5762df43fd37f9b
carlosfoliveira <carlos.fe.oliveira@gmail.com>
2021-03-22 14:35:49 -0300

    Chapter 03 - Injeçao de dependencia

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import './database';
+import './shared/container';
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 
@@ -8,6 +9,7 @@ import swaggerFile from './swagger.json';
 const app = express();
 
 app.use(express.json());
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
