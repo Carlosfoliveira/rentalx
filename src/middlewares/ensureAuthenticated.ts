982003f43c9233f18380a7c10e28b4ccb6c6c4d3
carlosfoliveira <carlos.fe.oliveira@gmail.com>
2021-03-22 18:40:07 -0300

    Chapter 03 - Usuario

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,40 @@
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import { AppError } from '../errors/AppError';
+import { UsersRepository } from '../modules/accounts/repositories/implementations/UsersRepository';
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('Token missing', 401);
+  }
+
+  const [, token] = authHeader.split(' ');
+  try {
+    const { sub: user_id } = verify(
+      token,
+      'f1fc2d60e419407b5d614a042bea1186'
+    ) as IPayload;
+
+    const usersRepository = new UsersRepository();
+    const user = usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User does not exists!', 401);
+    }
+
+    next();
+  } catch {
+    throw new AppError('Invalid token!', 401);
+  }
+}
