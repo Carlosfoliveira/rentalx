946e9c3a983ae73ea665b7166747ec886bd895c2
carlosfoliveira <carlos.fe.oliveira@gmail.com>
2021-03-23 20:30:21 -0300

    Chapter 03 - Avatar de usuario

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -27,12 +27,15 @@ export async function ensureAuthenticated(
     ) as IPayload;
 
     const usersRepository = new UsersRepository();
-    const user = usersRepository.findById(user_id);
+    const user = await usersRepository.findById(user_id);
 
     if (!user) {
       throw new AppError('User does not exists!', 401);
     }
 
+    request.user = {
+      id: user_id,
+    };
     next();
   } catch {
     throw new AppError('Invalid token!', 401);
